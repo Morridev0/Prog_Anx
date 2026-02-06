f17beadcc89287a9408fa57f8865829cdacdcc08
Morridev0 <morri.dev0@gmail.com>
2026-02-06 20:58:39 +0100

    instrucciones

--- scripts/pick_falling_item_minigame.gd
+++ scripts/pick_falling_item_minigame.gd
@@ -10,3 +10,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+# Falta añadir que si coges la botella pierdas puntos y lógica distinta para cada uno de los demás objetos ***
